c96123597b8e50a9bf4029e4af03224b89809241
mathfunction <mathfunction@gmail.com>
2020-08-13 22:21:35 +0800

    2020

---
+++ encodingFun/test.cpp
@@ -0,0 +1,21 @@
+
+#include<iostream>
+
+
+using namespace std;
+
+
+
+int main(){
+	
+	for(int i=0;i<1;i++){
+		unsigned char d = 2;
+		
+		cout << (d << i) << endl;;
+	}//endfor
+	
+
+
+
+	return 0;
+}//end_main
